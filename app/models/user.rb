47db75235bb1e398bd28f1eee6e48b1fb73122d7
Ryuki-08 <ryu.0219.ch@gmail.com>
2021-02-25 20:11:09 +0900

    userモデルにバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :family_name, presence: true, format: { with: /^[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /^[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kana, presence: true, format: { with: /^([ァ-ン]|ー)+$/}
+  validates :first_name_kana, presence: true, format: { with: /^([ァ-ン]|ー)+$/}
+  validates :birthday, presence: true
+  validates :encrypted_password, confirmation: true, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+
+  
 end
