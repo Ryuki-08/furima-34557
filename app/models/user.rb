0edc1644218d29047ae83d8020bfcd5fac993159
Ryuki-08 <ryu.0219.ch@gmail.com>
2021-02-27 19:11:50 +0900

    ユーザー認証機能導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,13 @@ class User < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :family_name, presence: true, format: { with: /^[ぁ-んァ-ン一-龥]/}
-  validates :first_name, presence: true, format: { with: /^[ぁ-んァ-ン一-龥]/}
-  validates :family_name_kana, presence: true, format: { with: /^([ァ-ン]|ー)+$/}
-  validates :first_name_kana, presence: true, format: { with: /^([ァ-ン]|ー)+$/}
-  validates :birthday, presence: true
-  validates :encrypted_password, confirmation: true, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
-
-  
+  with_options presence: true do
+    validates :nickname
+    validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+    validates :family_name_kana, format: { with:/\A[ァ-ヶー－]+\z/}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :birthday
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  end
 end
