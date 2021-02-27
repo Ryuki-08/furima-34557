dce6ce9c20c5c362f15f1ff83d1c0401bce8076a
Ryuki-08 <ryu.0219.ch@gmail.com>
2021-02-27 20:34:47 +0900

    ログイン、ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
     validates :nickname
     validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
-    validates :family_name_kana, format: { with:/\A[ァ-ヶー－]+\z/}
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
     validates :birthday
     validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
