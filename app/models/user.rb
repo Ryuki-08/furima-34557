82435e2014614b37226985c6c0561029c7a06233
Ryuki-08 <ryu.0219.ch@gmail.com>
2021-03-02 11:22:29 +0900

    レビュー修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
     validates :birthday
+  end
+
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options format: { with: /\A[ァ-ヶー－]+\z/} do
+    validates :family_name_kana
+    validates :first_name_kana
   end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
