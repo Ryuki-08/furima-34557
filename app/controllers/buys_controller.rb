c4e3ec14f5a82f28f921def57b018e10f6741a01
Ryuki-08 <ryu.0219.ch@gmail.com>
2021-03-10 21:47:01 +0900

    カード決済機能の実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -2,23 +2,36 @@ class BuysController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @buys = Buy.all
+    @buys = BuyAddress.new
     @item = Item.find(params[:item_id])
   end
 
   def create
-    @buys = Buy.create(buy_params)
-    Address.create(address_params)
+    @buys = BuyAddress.new(buy_address_params) # create = new + save
+    if @buys.valid?
+      pay_item
+      @buys.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
   end
 
   private
 
-  def buy_params
-    params.permit(:item_id).merge(user_id: current_user.id)
+  def buy_address_params
+    params.require(:buy_address).permit(:postal_code, :prefectures_id, :municipality, :building_name, :house_number, :phone_number,).merge(item_id: params[:item_id], token: params[:token], user_id: current_user.id)
   end
 
-  def address_params
-    params.permit(:postal_code, :prefectures_id, :municipality, :building_name, :house_number, :phone_number,).merge(buy_id: @buy.id)
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buy_address_params[:token],
+      currency: 'jpy'
+    )
   end
 
 
